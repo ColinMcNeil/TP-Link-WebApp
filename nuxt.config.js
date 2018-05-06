96f52740b126c586fd0e7a23891a6853b6614075
Colin <colinmcneil@gmail.com>
2018-05-05 22:43:47 -0400

    Remove google auth for now

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,22 @@
 module.exports = {
     //plugins: ['~/plugins/vue-google-auth'],
     router: {
-        middleware: ['auth'],
+        //middleware: ['auth'],
         base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/TP-Link-WebApp/' : ''
     },
     modules: [
         '@nuxtjs/axios',
-        '@nuxtjs/auth',
+        //'@nuxtjs/auth',
         'nuxt-fontawesome'
     ],
     auth: {
         strategies: {
             google: {
                 client_id: '811800195268-dcspgc96q1rt4kdp1sgk7nmemsg745fv.apps.googleusercontent.com'
-            },
-            redirect: {
-                callback: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/TP-Link-WebApp/login' : ''
             }
+        },
+        redirect: {
+            callback: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/TP-Link-WebApp/login' : ''
         }
     },
     plugins: [
