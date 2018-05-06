3e6a3de32f38f87bad0223fdde079094a4bc2424
Colin <colinmcneil@gmail.com>
2018-05-05 22:11:58 -0400

    Remove vue-google-auth entirely

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,6 @@ module.exports = {
         }
     },
     plugins: [
-        '~/plugins/vue-google-auth',
         '~/plugins/vue-cookie',
     ],
     build: {
--- plugins/vue-google-auth.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import GoogleAuth from 'vue-google-auth';
-
-Vue.use(GoogleAuth, {
-    clientId: '811800195268-dcspgc96q1rt4kdp1sgk7nmemsg745fv.apps.googleusercontent.com' })
